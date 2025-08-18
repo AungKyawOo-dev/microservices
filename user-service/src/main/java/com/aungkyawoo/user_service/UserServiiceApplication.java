7883a4cec6c454c54b4ac3bf189b1af6ff40a068
unknown <aungkyawoo.mtl@gmail.com>
2025-08-18 13:51:28 +0700

    user service initial

---
+++ user-service/src/main/java/com/aungkyawoo/user_service/UserServiiceApplication.java
@@ -0,0 +1,13 @@
+package com.aungkyawoo.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiiceApplication.class, args);
+	}
+
+}
