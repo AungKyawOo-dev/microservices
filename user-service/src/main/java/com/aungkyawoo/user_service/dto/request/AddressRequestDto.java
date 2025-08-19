085f861b41084ffd1aaac4ba207cfd2b35a7221f
unknown <aungkyawoo.mtl@gmail.com>
2025-08-19 16:00:44 +0700

    add validations

--- user-service/src/main/java/com/aungkyawoo/user_service/dto/request/AddressRequestDto.java
+++ user-service/src/main/java/com/aungkyawoo/user_service/dto/request/AddressRequestDto.java
@@ -1,5 +1,7 @@
 package com.aungkyawoo.user_service.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +12,18 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class AddressRequestDto {
+    @NotEmpty(message = "Line 1 should not be empty")
     private String line1;
     private String line2;
+    @NotEmpty(message = "City should not be empty")
     private String city;
+    @NotEmpty(message = "State should not be empty")
     private String state;
+    @NotEmpty(message = "Postal code should not be empty")
+    @Pattern(regexp = "[0-9]{5}", message = "Postal code should be 5 digits")
     private String postalCode;
+    @NotEmpty(message = "Country should not be empty")
     private String country;
+    @NotEmpty(message = "Is default should not be empty")
     private Boolean isDefault;
 }
