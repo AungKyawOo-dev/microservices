41f0f29e856b56e3fcf6423d8d700411a9bb6736
unknown <aungkyawoo.mtl@gmail.com>
2025-08-20 16:44:25 +0700

    product service setup

---
+++ product-service/src/test/java/com/aungkyawoo/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aungkyawoo.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
